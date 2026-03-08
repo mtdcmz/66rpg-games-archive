6b02a8f0d04ce5a042794ec0181c426c6e4c746c
mtdcmz <meitadechangmingzi@gmail.com>
2026-03-08 17:38:41 +0800

    Auto-sync: 2026-03-08 17:38

--- data/hfplayer/js/Cloud/IFrameSave.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by heshang on 2016/9/9.
- */
-function IFrameSave(){
-    //½âÎö
-    this.ParseFrameInfo = function (index,currentFrameInfo) {}
-    //´ò°ü
-    this.DeParseFrameInfo = function (index,currentFrameInfo) {}
-}
